a8b942a30610a82bbd064d98f1a412ef6a971b2f
ahmed sayed <ahmedsaid50asr@gmail.com>
2025-10-19 21:38:01 +0300

    create search icon and customize app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widegts/customize_appbar.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+        margin: EdgeInsets.all(10),
+        child: Column(children: [CustomizeAppBar()]),
+      ),
+    );
   }
 }
